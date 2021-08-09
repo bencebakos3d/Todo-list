8e52163ef9d411d42e7f799fca0a1e0c42b115b1
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2021-08-09 18:40:38 +0200

    Add files via upload

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#todoAdd').on('click', () => {
         var newItem = $("#todoText").val();
         if (newItem.length>0) {
-            $("#todoList").append("<li>"+newItem+"</li>+<input type='checkbox'>"); 
+            $("#todoList").append("<div class='listElement'><li>"+newItem+"</li>+<input type='checkbox' class='checkbox'></div>"); 
             $("#todoAdd").val('')
         }
         else {
